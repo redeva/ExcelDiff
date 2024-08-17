e9768ac848fa973ad7e68acf09e85c2f29dc3fad
redeva <liyu5032@163.com>
2024-08-17 22:19:41 +0800

    第二次提交，修改输出文件名为时间格式，改正了输出提示中一个小问题

--- ExcelDiff.py
+++ ExcelDiff.py
@@ -1,6 +1,7 @@
 import openpyxl as ol
 from openpyxl.styles import PatternFill
 from openpyxl.styles import Font
+import time
 
 # 填充颜色
 yellow_fill = PatternFill(patternType='solid', fgColor='FFFF00')  # 黄
@@ -11,12 +12,12 @@ blue_fill = PatternFill(patternType='solid', fgColor='00CCFF')
 red_font = Font(color='c00000')  # 红色字体
 
 # 获取工作簿对象实例
-book1 = ol.load_workbook('20240724_105656_浅层2.xlsx')
+book1 = ol.load_workbook('8.xlsx')
 # book2 = ol.load_workbook('2.xlsx')
 
 # 获取工作表对象实例
-book1_sheet1 = book1['原始数据查看']
-book1_sheet2 = book1['异常值剔除后完整列表']
+book1_sheet1 = book1['Sheet1']
+book1_sheet2 = book1['Sheet2']
 
 # 定义两个list ，存放工作表待处理的单元格范围
 book1_sheet1_rowlist = range(book1_sheet1.min_row, book1_sheet1.max_row)  # 起始行加1 ，跳过标题行
@@ -30,31 +31,12 @@ for j in book1_sheet1_rowlist:
         if value_a != value_b:
             print('\n=============================\n')
             print(
-                f'出大事了，{value_a}和{value_a}的数值不一样！它们位于第{j}行第{k}列，对应的样品编号是{book1_sheet1.cell(row=j, column=1).value}，对应测试指标是{book1_sheet1.cell(row=1, column=k).value}')
+                f'出大事了，{value_a}和{value_b}的数值不一样！它们位于第{j}行第{k}列，对应的样品编号是{book1_sheet1.cell(row=j, column=1).value}，对应测试指标是{book1_sheet1.cell(row=1, column=k).value}')
             # 设置字体颜色及填充
             book1_sheet1.cell(row=j, column=k).font = red_font
             book1_sheet1.cell(row=j, column=k).fill = green_fill
 
 # 最后，将book1另存成一个文件
-book1.save('a1.xlsx')
-
-'''
-
-
-raw_excel = 'excel_raw.xlsx'
-result_excel = 'result.xlsx'
-workbook = load_workbook(raw_excel)
-writer = pd.ExcelWriter(result_excel , engine='openpyxl')
-writer.book = workbook
-
-df_1.to_excel(writer, sheet_name='test1_sheet')
-df_2.to_excel(writer, sheet_name='test2_sheet')
-
-writer.save()
-writer.close()
-
-
-
-
-
-'''
+save_time = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+excel_name = save_time  + '.xlsx'  # 由时间命名文件名，避免重复
+book1.save(excel_name)
